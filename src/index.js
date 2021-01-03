5a73ae53fdc1ccb0e156abe4380b1e90250e676e
sai-karthik-p <54533075+sai-karthik-p@users.noreply.github.com>
2021-01-03 22:09:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+var birthday = document.querySelector("#birthday");
+var luckyNumber = document.querySelector("#lucky-number");
+var checkBtn = document.querySelector("#btn-check");
+var output = document.querySelector("#output");
+var gifOutput = document.querySelector("#gif-output");
+
+function calculate(n) {
+  let sum = 0;
+  let remainder = 0;
+  while (n > 0) {
+    const current = n % 10;
+    sum += current;
+    n = Math.floor(n / 10);
+  }
+
+  remainder = sum % Number(luckyNumber.value);
+
+  if (remainder === 0) {
+    output.innerText = "Your Birthday is LUCKY!";
+    gifOutput.innerHTML = `<iframe src="https://giphy.com/embed/LWhLL32XDmcJhXTHVu" width="350" height="360" frameBorder="0" class="giphy-embed"></iframe>`;
+  } else {
+    output.innerText = "Oops! No luck.";
+    gifOutput.innerHTML = `<iframe src="https://giphy.com/embed/26tOXgoz0WNQhwb04" width="350" height="360" frameBorder="0" class="giphy-embed"></iframe>`;
+  }
+}
+
+function handleClick() {
+  var dateValue = birthday.value;
+
+  dateValue = dateValue.replace(/-/g, "");
+  dateValue = parseInt(dateValue, 10);
+
+  calculate(dateValue);
+}
+
+checkBtn.addEventListener("click", handleClick);
